a55daba5494af8f29be113f4bd46a9851146db8b
cauaKilou <caua.oliversouza@gmail.com>
2025-08-08 17:55:21 -0300

    aplication att4

--- src/main/java/br/ce/wcaquino/tasksfrontend/TasksFrontendApplication.java
+++ src/main/java/br/ce/wcaquino/tasksfrontend/TasksFrontendApplication.java
@@ -2,19 +2,12 @@ package br.ce.wcaquino.tasksfrontend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.FilterRegistrationBean; // Importe esta classe
-import org.springframework.boot.web.servlet.support.ErrorPageFilter; // Importe esta classe
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean; // Importe esta classe
+// Importe a classe de configuração a ser excluída
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
-@SpringBootApplication
-public class TasksFrontendApplication extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(TasksFrontendApplication.class);
-    }
+// Adicione o "exclude" na anotação @SpringBootApplication
+@SpringBootApplication(exclude = ErrorMvcAutoConfiguration.class)
+public class TasksFrontendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TasksFrontendApplication.class, args);
